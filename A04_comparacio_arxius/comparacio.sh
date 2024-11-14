cd905b4ec54c9d67e29495414c9b73dda3266179
Nicolas Coello <2023_nicolas.coello@iticbcn.cat>
2024-11-14 21:49:49 +0100

    Excercici scripts comparació

--- A04_comparacio_arxius/comparacio.sh
+++ A04_comparacio_arxius/comparacio.sh
@@ -3,14 +3,11 @@ mida_1=$(ls -l $1 | cut -d ' ' -f 5)
 mida_2=$(ls -l $2 | cut -d ' ' -f 5)
 nombre_fichero1=$1
 nombre_fichero2=$2
-#echo $nombre_fichero1
-#echo $nombre_fichero2
-#echo $mida_1
-#echo $mida_2
-if [ $mida_1>$mida_2 ]; then
-echo $nombre_fichero1 es més gran que $nombre_fichero2
-elif [ $mida_2>$mida_1 ]; then
-echo $nombre_fichero2 es més gran que $nombre_fichero1
+
+if [ $mida_1 -gt $mida_2 ]; then
+	echo "$nombre_fichero1 es més gran que $nombre_fichero2: $mida_1 bytes"
+elif [ $mida_2 -gt $mida_1 ]; then
+	echo "$nombre_fichero2 es més gran que $nombre_fichero1: $mida_2 bytes"
 else
-echo $nombre_fichero1 i $nombre_fichero2 són de la mateixa mida
+	echo "Els arxius $nombre_fichero1 i $nombre_fichero2 són de la mateixa mida"
 fi 
